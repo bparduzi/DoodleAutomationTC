bbe832eb85eba62fee03306e77b5fef15b10ad19
Besartë Parduzi <besarte@frakton.com>
2022-06-09 20:42:35 +0200

    Added more funcitonality

--- Cypress/e2e/createGroupPoll.cy.js
+++ Cypress/e2e/createGroupPoll.cy.js
@@ -2,24 +2,38 @@
 import doodleHomePage from './pages/doodleHomePage'
 import createGroupPoll from '../e2e/pages/createGroupPoll'
 
-
-describe('Pom implementation', () => {
-    beforeEach (() =>{
+describe('Pom implementation', () =>{
+    before(() =>{
         cy.visit('/');
-        // cy.clearCookies('https://doodle.com');
-        // cy.getCookies().should('be.empty')
-    })
-    it('Navigate to create poll page', ()=> {
+    })   
+    it('Create group poll form', ()=>{
         doodleHomePage.createBtn();
-        createGroupPoll.acceptCookies();
+        cy.wait(1000)
+        createGroupPoll.acceptCookies();  
+         cy.get('.Input--dark').should('contain','Your name')     
         createGroupPoll.nameInput('Name and Surname');
-        createGroupPoll.emailInput('besarta@frakton.com')
+        cy.get('.MeetingMetadata__organizer-email').should('contain','Your email')   
+        function makeid(length){
+            var result           = '';
+            var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            for ( var i = 0; i < length; i++ ) {
+               result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+         }
+         console.log(makeid(5));
+        createGroupPoll.emailInput(makeid(6) + "@getnada.com")
+        cy.get('.MeetingMetadata__title').should('contain','Title')   
         createGroupPoll.titleInput('QA')
+        cy.get('.MeetingMetadata__description').should('contain','Description (optional)')     
         createGroupPoll.descriptionInput('loreum ispum')
+        cy.get('.MeetingMetadata__location').should('contain','Location (optional)')     
         createGroupPoll.locationInput('Prishtina')
         createGroupPoll.sublistId();
+        cy.get('.rbc-timeslot-group').eq(1).click({force:true});
+        cy.get('#MeetingTimesSection').should('contain','Add your times')     
         createGroupPoll.timeInput();
         createGroupPoll.createInviteButton();
     })
-    
 })
